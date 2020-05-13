0ba8f90002fdce65c6c460ac7082d5772db769ec
berkeoznalbant <berke.oznalbant@gmail.com>
2020-05-13 11:02:44 +0300

    Add files via upload

--- batCreator.py
+++ batCreator.py
@@ -1,11 +1,20 @@
 import os, winshell
+import sys
 from win32com.client import Dispatch
 a = winshell.my_documents()
 
-showList = ["Cosmos", "Dynasties", "From The Earth", "Rick And Morty", "Seven Worlds", "The Outsider", "The Planets",
-            "Westworld"]
+myArg = sys.argv
+if len(myArg) > 1:
+    showList = ''.join(sys.argv[-1])
+else:
+    showList = ["Cosmos", "Dynasties", "From The Earth", "Rick And Morty", "Seven Worlds", "The Outsider",
+                "The Planets",
+                "Westworld"]
+
 folderPath = 'C:/Users/berke/Documents/Shortcuts/'
-for show in showList:
+
+
+def createBat(folderPath, show):
     filePath = folderPath + show + '.bat'
     f = open(filePath, 'w')
     f.writelines('nircmd.exe setsysvolume 65535')
@@ -14,14 +23,21 @@ for show in showList:
     # f.writelines('\npy C:/Users/berke/PycharmProjects/cortanaSeries/main.py %s%s%s' %["\"", show, "\""])
     f.close()
 
-    path = os.path.join("C:\ProgramData\Microsoft\Windows\Start Menu\Programs", show + ".lnk")
+    path = os.path.join("C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Cortana Commands", show + ".lnk")
     target = r"C:\Users\berke\Documents\Shortcuts/" + show + '.bat'
     wDir = r"C:\Users\berke\Documents\Shortcuts"
-    icon = r"C:\Program Files (x86)\K-Lite Codec Pack\MPC-HC64/mpc-hc64_nvo"
+    icon = r"C:\Program Files (x86)\K-Lite Codec Pack\MPC-HC64/mpc-hc64_nvo.exe"
     shell = Dispatch('WScript.Shell')
     shortcut = shell.CreateShortCut(path)
     shortcut.Targetpath = target
     shortcut.WorkingDirectory = wDir
     shortcut.IconLocation = icon
     shortcut.save()
-    pass
+
+
+if isinstance(showList, str):
+    createBat(folderPath,showList)
+else:
+    for show in showList:
+        createBat(folderPath, show)
+
--- main.py
+++ main.py
@@ -20,7 +20,7 @@ myArg = sys.argv
 if len(myArg) > 1:
     showName = ''.join(sys.argv[-1])
 else:
-    showName = "Rick and morty"
+    showName = "Seinfeld"
 
 textPath = "C:/Users/berke/Documents/Shortcuts/%s.txt" % showName
 foundInTextFile = os.path.isfile(showName + ".txt")
@@ -33,13 +33,14 @@ for show in os.listdir("E:/Diziler"):
 
 showFolderPath = "E:/Diziler/%s/" % folderName
 currEpInd = -1
-episodeList = glob.glob(showFolderPath + "**/*.mkv", recursive=True)
+# episodeList = glob.glob([showFolderPath + "**/*.mkv", showFolderPath + "**/*.mp4"], recursive=True)
+episodeList = [item for sublist in [glob.glob(showFolderPath + ext, recursive=True) for ext in ["**/*.mp4", "**/*.mkv"]] for item in sublist]
 
-registry_key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, "Software\MPC-HC\MPC-HC\Recent File List", 0,
+registry_key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, "Software\MPC-HC\MPC-HC\Settings", 0,
                               winreg.KEY_READ)
 
 for i in range(40):
-    value, regtype = winreg.QueryValueEx(registry_key, "File" + str(i + 1))
+    value, regtype = winreg.QueryValueEx(registry_key, "File Name " + str(i))
     if re.search(showName, value, re.IGNORECASE):
         foundInReg = True
         fullPath = value
@@ -58,7 +59,7 @@ if foundInReg:
         if fileName in episode:
             episodeDur = VideoFileClip(fullPath).duration
             isLastEpisode = currEpInd + 1 >= len(episodeList)
-            isWatched = lastPosDuration / episodeDur > 0.9
+            isWatched = lastPosDuration / episodeDur > 0.9 or lastPosDuration == 0
             break
     if isWatched and not isLastEpisode:
         currEpInd += 1
@@ -71,7 +72,9 @@ else:
     currEpInd = 0
 
 currEpPath = episodeList[currEpInd]
-
+# myCommand ="\"%s\" %s" % (, "/fullscreen")
+# fullCommand = '\"C:/Program Files (x86)/K-Lite Codec Pack/MPC-HC64/mpc-hc64_nvo.exe\" ' + myCommand
+# os.system(fullCommand)
 subprocess.Popen(["C:/Program Files (x86)/K-Lite Codec Pack/MPC-HC64/mpc-hc64_nvo.exe", currEpPath])
 
 f = open(textPath, 'w')
